7dfebdaf02ba7bac740e8f8d28bf30356a40db95
Uthpala Heenatigala <uthpala@passionatepeople.io>
2025-02-06 10:54:04 +0100

    remove unused plugins

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,9 @@
+return {
+  { "RRethy/vim-illuminate", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "mfussenegger/nvim-dap-python", enabled = false },
+  { "danymat/neogen", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
+  { "echasnovski/mini.icons", enabled = false },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,6 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    indent = { enable = false }, -- Disable Treesitter indentation guides
+  },
+}
